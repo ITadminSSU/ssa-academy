5899a0575ee2b18b1a3b3e94df86b74dd75706d3
ITadminSSU <286199062+ITadminSSU@users.noreply.github.com>
2026-08-18 19:43:15 +0800

    Update facebook-group-button.tsx

--- resources/js/pages/course-player/partials/facebook-group-button.tsx
+++ resources/js/pages/course-player/partials/facebook-group-button.tsx
@@ -4,7 +4,10 @@ import { Facebook } from 'lucide-react';
 const FACEBOOK_GROUP_URL = 'https://www.facebook.com/share/g/14ttXqLttek/';
 
 const FacebookGroupButton = () => (
-   <Button asChild variant="outline">
+   <Button
+      asChild
+      className="border-0 bg-[#1877F2] text-white hover:bg-[#166FE5] hover:text-white"
+   >
       <a href={FACEBOOK_GROUP_URL} target="_blank" rel="noopener noreferrer">
          <Facebook className="h-4 w-4" />
          Click here to join our Facebook group
